2439109a6c5e9b55abf324fbd051ed3889a144fa
roshanharry <roshandhoni202@gmail.com>
2024-05-15 00:59:51 +0530

    Deployment

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,10 +11,14 @@ import usersRoutes from "./routes/users.routes.js";
 import connectDB from "./db/connect.js";
 import { app, server } from "./socket/socket.js";
 
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+
 
 
 
@@ -25,11 +30,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
 
+app.use(express.static(path.join(__dirname,"/Client/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname,"Client", "dist", "index.html"))
+})
 
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+// app.get('/', (req, res) => {
+//     res.send("Hello world");
+// });
 
 
 
